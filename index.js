6856d011b65e73c3ef85bb5dbbd1d4a7fe5bba76
alexanderdombroski <dom21013@byui.edu>
2024-10-17 15:10:20 -0600

    added timer countdown functions

--- index.js
+++ index.js
@@ -5,3 +5,23 @@ function trickOrTreat(event) {
         : `<img src="candycorn.png" alt="treat candycorn">`
 }
 
+
+(function createCoundown() {
+    const halloween = new Date('October 31, 2024 00:00:00').getTime();
+    
+    function updateTimer() {
+
+        const timeLeft = halloween - new Date().getTime();
+        
+        const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+        
+        // Display the countdown
+        document.getElementById('countdown').innerText = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+    }
+
+    // Update the countdown every second
+    const timer = setInterval(updateTimer, 1000);
+})();
